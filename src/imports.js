972b36b340ad2d5cecbd67d7da1ba3b105e3e452
Dexter Pacaña <dextepacanya@gmail.com>
2024-12-08 13:42:24 +0800

    Delete src directory

--- src/imports.js
+++
@@ -1,9 +0,0 @@
-const importmap = document.createElement('script');
-importmap.type = 'importmap';
-importmap.textContent = JSON.stringify({
-    imports: {
-        "three": "https://cdn.jsdelivr.net/npm/three@0.152.0/build/three.module.js",
-        "three/addons/": "https://cdn.jsdelivr.net/npm/three@0.152.0/examples/jsm/"
-    }
-});
-document.currentScript.after(importmap);
